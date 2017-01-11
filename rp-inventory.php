4e4db7ea993bca0219af6b6c34d018192e9397b4
wallnerk <klemens.wallner@avl.com>
2017-01-11 12:31:07 +0100

    added activation/deactivation

--- rp-inventory.php
+++ rp-inventory.php
@@ -10,6 +10,9 @@ Text Domain: rp-inventory
 */ 
 
 function rp_inventory_shortcode($atts, $content) {
+   	global $wpdb;
+  	global $db_table_name;
+
 	extract(shortcode_atts(array(
 		'title' => __('RP Inventory', 'rp-inventory'),
 		'name' => 'name',
@@ -18,12 +21,57 @@ function rp_inventory_shortcode($atts, $content) {
 
 	$title = esc_attr($title);
 
+    $db_result = print_r($wpdb->get_results("show tables"));
+
 	$output  = "\n<div class=\"rp-inventory\">\n";
-	$output .= $name;
+	$output .= $name . " is " . $db_result;
 	$output .= "\n</div>\n";
 
 	return $output;
 }
 add_shortcode ('rp-inventory', 'rp_inventory_shortcode');
+
+
+
+
+
+
+
+$db_table_name = $wpdb->prefix . 'rp_inventory';
+ 
+// function to create the DB / Options / Defaults					
+function rp_inventory_install() {
+   	global $wpdb;
+  	global $db_table_name;
+ 
+	// create the ECPT metabox database table
+	if($wpdb->get_var("show tables like '$db_table_name'") != $db_table_name) 
+	{
+		$sql = "CREATE TABLE " . $db_table_name . " (
+		`id` mediumint(9) NOT NULL AUTO_INCREMENT,
+		`field_1` mediumtext NOT NULL,
+		`field_2` tinytext NOT NULL,
+		`field_3` tinytext NOT NULL,
+		`field_4` tinytext NOT NULL,
+		UNIQUE KEY id (id)
+		);";
+ 
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+ 
+    register_uninstall_hook( __FILE__, 'rp_inventory_uninstall' );
+}
+
+function rp_inventory_uninstall() {
+    global $wpdb;
+    global $db_table_name;
+
+    // delete the database table
+    $wpdb->query("DROP TABLE IF EXISTS " . $db_table_name);
+}
+
+// run the install scripts upon plugin activation
+register_activation_hook(__FILE__,'rp_inventory_install');
 
 ?>
